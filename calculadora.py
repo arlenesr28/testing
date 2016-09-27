fee7845eb9b9fc129bd3d722521b86f7130d2069
arlenesr28 <arlene.sr28@gmail.com>
2016-09-27 14:56:49 -0600

    Poniendo la division como debe de ser

--- calculadora.py
+++ calculadora.py
@@ -8,7 +8,7 @@ def Menu():
                 1) Suma
                 2) Resta
                 3) Multiplicacion
-                4) Division ///////////////******************/////////////////////
+                4) Division 
                 5) Salir
           """
 
