1295cd141d6f86cce2b5d3d7804ffc3d8a7e8806
arlenesr28 <arlene.sr28@gmail.com>
2016-09-27 12:08:08 -0600

    quitando signos de multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -7,7 +7,7 @@ def Menu():
                 ------------
                 1) Suma
                 2) Resta
-                3) Multiplicacion **********************
+                3) Multiplicacion 
                 4) Division
                 5) Salir
           """
